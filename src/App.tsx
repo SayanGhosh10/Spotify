37ff971f941643c028e750a822fd685f0423da08
SayanGhosh10 <Sayanmyself50@gmail.com>
2024-02-26 23:02:58 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView >
+      <StatusBar />
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
